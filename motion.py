92c9489ee863bf51c4a601f931289aae8472698f
Aidan <aidanmadge@gmail.com>
2026-02-18 22:03:53 +0000

    angle error constant

--- motion.py
+++ motion.py
@@ -16,7 +16,7 @@ WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * PI
 WHEELBASE_WIDTH = 152
 
 DISTANCE_ERROR = -0.84 * PI # Making it bigger makes it go less far
-ANGLE_ERROR = -3.074 # Making it bigger makes it turn more
+ANGLE_ERROR = 6.16 # Making it bigger makes it turn more
 
 MINI_WAIT_TIME = 0.15  # Time to wait after each movement (Seconds)
 
