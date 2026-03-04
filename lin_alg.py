8d5fc79a866fd2f6d88efd875dca86d838c5b0c3
LawanF <fathullahlawan@gmail.com>
2026-03-04 18:38:25 +0000

    WIP: planning

---
+++ lin_alg.py
@@ -0,0 +1,18 @@
+import math
+
+def dot_prod(a_x, a_y, b_x, b_y):
+    return a_x * b_x + a_y * b_y
+
+def magnitude(x, y):
+    return math.sqrt(x**2 + y**2)
+
+def projection(a_x, a_y, b_x, b_y):
+    """
+    Project vector b onto vector a
+    """
+    dot = dot_prod(a_x, a_y, b_x, b_y)
+    a_mag = magnitude(a_x, a_y)
+    
+    proj_fac = dot / a_mag
+
+    return proj_fac * a_x, proj_fac * a_y
--- planning.py
+++ planning.py
@@ -1,15 +1,24 @@
 import brickpi3
-import time
 import math
 import cv2
 import numpy as np
 from picamera2 import Picamera2
 from picameracans import captureCanCentroids, displayImg, WHITE, FONT
 from picamerahomographygrid import drawGridOnImage, HtransformXYtoUV, HtransformUVtoXY, HInv
-# from motion import forward, turnAntiClockwise
 
-B_UNCERTAINTY = 2
+from lin_alg import projection, magnitude
+
+BARRIER_RADIUS = 3.3
 RADIUS = 2
+ROBOT_RADIUS = 10
+
+SEMICIRCLE_FIDELITY = 11 # number of evaluated points on the evaluated the circle
+SEMICIRCLE_RADIUS = 5 # in cm
+SEMICIRCLE_RANGE = 180 # range of angles in the semicircle
+SEMICIRCLE_STEP = SEMICIRCLE_RANGE / (SEMICIRCLE_FIDELITY - 1)
+
+CAN_X_UNCERTAINTY = 3
+CAN_Y_UNCERTAINTY = 15
 
 # Needed constants
 # TODO: Set these constants.
@@ -45,62 +54,49 @@ theta = 0.0
 
 # Barrier (obstacle) locations
 # A barrier should be a tuple (x, y), where (x, y) is the centre of the barrier. Barriers are not added to this list until seen with the camera.
-barriers = []
+# barriers = []
 
 # Set an initial target location which is beyond the obstacles
 # TODO: Set this variable to something accurate to the location of the target in the real life course.
 target = (0, 400)
 
-# I think can be replaced with our own position / odometry functions
-# Function to predict new robot position based on current pose and velocity controls
-# Uses time deltat in future
-# Returns xnew, ynew, thetanew
-# Also returns path. This is just used for graphics, and returns some complicated stuff
-# used to draw the possible paths during planning. Don't worry about the details of that.
-def predictPosition(vL, vR, x, y, theta, deltat):
-    # TODO: Change this function to get rid of drawing/pygame references and variables we don't need!
-    # Simple special cases
-    # Straight line motion
-    if (vL == vR): 
-        xnew = x + vL * deltat * math.cos(theta)
-        ynew = y + vL * deltat * math.sin(theta)
-        thetanew = theta
-        path = (0, vL * deltat)   # 0 indicates pure translation
-    # Pure rotation motion
-    elif (vL == -vR):
-        xnew = x
-        ynew = y
-        thetanew = theta + ((vR - vL) * deltat / W)
-        path = (1, 0) # 1 indicates pure rotation
-    else:
-        # Rotation and arc angle of general circular motion
-        # Using equations given in Lecture 2
-        R = W / 2.0 * (vR + vL) / (vR - vL)
-        deltatheta = (vR - vL) * deltat / W
-        xnew = x + R * (math.sin(deltatheta + theta) - math.sin(theta))
-        ynew = y - R * (math.cos(deltatheta + theta) - math.cos(theta))
-        thetanew = theta + deltatheta
+def semicircle(x = 0, y = 0, theta = 90):
+    if theta != 90:
+        print("semicircle, WARNING: ROBOT SHOULD BE FACING FORWARDS!")
+        
+    least_angle = theta - SEMICIRCLE_RANGE / 2
+    
+    semicircle_positions = []
+    
+    for i in range(SEMICIRCLE_FIDELITY):
+        cur_angle = least_angle + SEMICIRCLE_STEP * i
+        cur_rads = math.radians(cur_angle)
+        
+        new_x = x + SEMICIRCLE_RADIUS * math.cos(cur_rads)
+        new_y = y + SEMICIRCLE_RADIUS * math.sin(cur_rads)
+        
+        semicircle_positions.append((new_x, new_y))
+    
+    return semicircle_positions
 
-        # To calculate parameters for arc drawing (complicated Pygame stuff, don't worry)
-        # We need centre of circle
-        (cx, cy) = (x - R * math.sin(theta), y + R * math.cos (theta))
-        # Turn this into Rect
-        Rabs = abs(R)
-        ((tlx, tly), (Rx, Ry)) = ((int(u0 + k * (cx - Rabs)), int(v0 - k * (cy + Rabs))), (int(k * (2 * Rabs)), int(k * (2 * Rabs))))
-        if (R > 0):
-            start_angle = theta - math.pi/2.0
-        else:
-            start_angle = theta + math.pi/2.0
-        stop_angle = start_angle + deltatheta
-        path = (2, ((tlx, tly), (Rx, Ry)), start_angle, stop_angle) # 2 indicates general motion
-
-    return (xnew, ynew, thetanew, path)
+def scorePosition(x = 0, y = 0, new_x, new_y, barriers):
+    closest_barrier = calculateClosestObstacleDistance(x, y, barriers)
+    c_x, c_y = closest_barrier # assuming x = 0, y = 0 for the can coordinates
+    new_x -= x
+    new_y -= y
+    proj_x, proj_y = projection(new_x, new_y, c_x, c_y)
+    distance = magnitude(proj_x - c_x, proj_y - c_y)
+    
+    # TODO:
+    
+    
 
 # Function to calculate the closest obstacle at a position (x, y)
 # Used during planning
-def calculateClosestObstacleDistance(x, y):
+def calculateClosestObstacleDistance(x, y, barriers):
     # TODO: Remove if statement about whether we know about the barrier or not.
-    closestdist = 100000.0  
+    closest_dist = float("inf")
+    closest_barrier = None
     # Calculate distance to closest obstacle
     for barrier in barriers:
         # Is this a barrier we know about? barrier[2] flag is set when sonar observes it
@@ -109,10 +105,11 @@ def calculateClosestObstacleDistance(x, y):
             dy = barrier[1] - y
             d = math.sqrt(dx**2 + dy**2)
             # Distance between closest touching point of circular robot and circular barrier
-            dist = d - BARRIERRADIUS - ROBOTRADIUS
-            if (dist < closestdist):
-                    closestdist = dist
-    return closestdist
+            dist = d - BARRIER_RADIUS - ROBOT_RADIUS
+            if (dist < closest_dist):
+                closest_dist = dist
+                closest_barrier = barrier
+    return closest_barrier
 
 
 # Main loop
@@ -156,53 +153,9 @@ while(1):
 
     displayImg(img) # Prints to web interface
 
-    
-
     # Planning
     # We want to find the best benefit where we have a positive component for closeness to target,
     # and a negative component for closeness to obstacles, for each of a choice of possible actions
-
-    bestBenefit = -100000
-    FORWARDWEIGHT = 12
-    OBSTACLEWEIGHT = 16
-
-    # TODO: Change this loop to get rid of references to drawing and pygame variables we don't need, and use our own modified above functions
-    # Range of possible motions: each of vL and vR could go up or down a bit
-    vLpossiblearray = (vL - MAXACCELERATION * dt, vL, vL + MAXACCELERATION * dt)
-    vRpossiblearray = (vR - MAXACCELERATION * dt, vR, vR + MAXACCELERATION * dt)
-    print ("New action")
-    newpositionstodraw = [] # Also for possible plotting of robot end positions
-    for vLpossible in vLpossiblearray:
-        for vRpossible in vRpossiblearray:
-                # We can only choose an action if it's within velocity limits
-                if (vLpossible <= MAXVELOCITY and vRpossible <= MAXVELOCITY and vLpossible >= -MAXVELOCITY and vRpossible >= -MAXVELOCITY):
-                    # Predict new position in TAU seconds
-                    TAU = 1.5 
-                    (xpredict, ypredict, thetapredict, path) = predictPosition(vLpossible, vRpossible, x, y, theta, TAU)
-                    newpositionstodraw.append((xpredict, ypredict))
-                    # What is the distance to the closest obstacle from this possible position?
-                    distanceToObstacle = calculateClosestObstacleDistance(xpredict, ypredict)
-                    # Calculate how much close we've moved to target location
-                    previousTargetDistance = math.sqrt((x - target[0])**2 + (y - target[1])**2)
-                    newTargetDistance = math.sqrt((xpredict - target[0])**2 + (ypredict - target[1])**2)
-                    distanceForward = previousTargetDistance - newTargetDistance
-                    # Alternative: how far have I moved forwards?
-                    # distanceForward = xpredict - x
-                    # Positive benefit
-                    distanceBenefit = FORWARDWEIGHT * distanceForward
-                    # Negative cost: once we are less than SAFEDIST from collision, linearly increasing cost
-                    if (distanceToObstacle < SAFEDIST):
-                        obstacleCost = OBSTACLEWEIGHT * (SAFEDIST - distanceToObstacle)
-                    else:
-                        obstacleCost = 0.0
-                    # Total benefit function to optimise
-                    benefit = distanceBenefit - obstacleCost
-                    if (benefit > bestBenefit):
-                        vLchosen = vLpossible
-                        vRchosen = vRpossible
-                        bestBenefit = benefit
-    vL = vLchosen
-    vR = vRchosen
 
 picam2.stop()
     # TODO: move forward using vL and vR
