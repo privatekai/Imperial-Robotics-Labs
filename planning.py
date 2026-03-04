5872a8a6a0d3b525b2b40203f81e098d5bde2e45
Rukaiya Khondaker <rukaiya.nameera@gmail.com>
2026-03-04 21:35:09 +0000

    take into account loop time in time elapsed

--- planning.py
+++ planning.py
@@ -25,6 +25,9 @@ picam2.start()
 # Timestep delta to run control at
 dt = 0.25
 
+# Timestep it takes for loop to finish
+loop_time_ = 0.250
+
 # Target location (cm) — 4.5m ahead along y-axis
 target = (0, 450)
 
@@ -131,7 +134,7 @@ def dwa_choose_velocities(x, y, theta, vL, vR, verbose=False):
                 min_obstacle_dist = float('inf')
                 for s in range(1, 6):
                     t = TAU * s / 5
-                    xp, yp, _ = predictPosition(vLpossible, vRpossible, x, y, theta, t)
+                    xp, yp, _ = predictPosition(vLpossible, vRpossible, x, y, theta, t + loop_time_)
                     d = calculateClosestObstacleDistance(xp, yp)
                     if d < min_obstacle_dist:
                         min_obstacle_dist = d
