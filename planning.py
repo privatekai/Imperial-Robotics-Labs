02edc76feaca8d6ad6c2ae46aa7902fa042f1daa
Sani <sa4523@ic.ac.uk>
2026-03-04 20:14:44 +0000

    print selected angle

--- planning.py
+++ planning.py
@@ -139,13 +139,14 @@ while(1):
         (x,y,angle) = semicircle_positions[i]
         score = scorePosition(x, y, barriers)
 
-        print("x: " + str(x) + " y: " + str(y) + " angle: " + str(angle))
+        print(f"x: {x}, y: {y}, theta: {theta}")
         print("score: ", score)
         if score > best_score:
             best_score_index = i
             best_score = score
 
     _, _, angle = semicircle_positions[best_score_index]
+    print(f"SELECTED MOV - x: {x}, y: {y}, theta: {theta}")
 
     # TODO: Turn, move forward, Turn back
 
