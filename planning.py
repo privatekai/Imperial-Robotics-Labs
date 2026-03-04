11cb3ae332b3a56cc0f635882fde438cfcbbd7ac
Aidan <aidanmadge@gmail.com>
2026-03-04 23:37:13 +0000

    optimise loop

--- planning.py
+++ planning.py
@@ -95,7 +95,7 @@ def dwa_choose_velocities(x, y, theta, vL, vR, verbose=False):
     vLchosen = vL
     vRchosen = vR
 
-    steps = [i * MAXACCELERATION * dt for i in range(-4, 5)]
+    steps = [i * MAXACCELERATION * dt for i in range(-2, 4)]
     vLpossiblearray = list(set([vL + s for s in steps if vL + s != 0]))
     vRpossiblearray = list(set([vR + s for s in steps if vR + s != 0]))
 
