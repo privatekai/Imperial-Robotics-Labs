a918efa881933149f534e5666cc86a00a23e9d7b
Sani <sa4523@ic.ac.uk>
2026-03-05 09:52:02 +0000

    puts speed at 1000

--- motion.py
+++ motion.py
@@ -6,7 +6,7 @@ import brickpi3
 
 # UNITS ARE MILLIMETRES
 
-MOVEMENT_SPEED = 900  # Speed for moving forward (Degrees Per Second)
+MOVEMENT_SPEED = 1000  # Speed for moving forward (Degrees Per Second)
 TURNING_SPEED = 300   # Speed for turning (Degrees Per Second)
 
 PI = 3.14159627
